441d54d684c1528a6cb6b7fac4631a0bb0caa722
Nick Jones <NRJones0210@gmail.com>
2015-10-29 20:55:28 -0600

    Moved functions into lib folder, but now heroku is not working

--- routes/orders.js
+++ routes/orders.js
@@ -12,59 +12,6 @@ var OrdersFunctions = require('../lib/orders')
 require('dotenv').load()
 /* GET home page. */
 
-var joinOrderUser = function (orders, users) {
-  orders.forEach(function (order) {
-    users.forEach(function (user) {
-      if(order.userId.toString() === user._id.toString()) {
-        order.user = user
-      }
-    })
-  })
-  return orders
-}
-
-var joinOrderItems = function (orders, orderItems) {
-  var indexed = orderItems.reduce(function (result, orderItem) {
-    result[orderItem._id.toString()] = orderItem
-    return result
-  }, {})
-  orders.forEach(function (order) {
-    order.orderItems = order.orderItemIds.map(function (_id) {
-      return indexed[_id.toString()]
-    })
-  })
-  return orders
-}
-
-// var joinOrderPizzas = function (orders, orderPizzas) {
-//   var indexed = orderPizzas.reduce(function (result, orderPizza) {
-//     result[orderPizza._id.toString()] = orderPizza
-//     return result
-//   }, {})
-//   orders.forEach(function (order) {
-//     orders.orderItems.pizzas = order.orderItemIds.pizzas.map(function (_id) {
-//       return indexede[_id.toString()]
-//     })
-//   })
-//   return orders
-// }
-
-var joinOrderItemsToppings = function (orders, toppings) {
-  var indexed = toppings.reduce(function (result, topping) {
-    result[topping._id.toString()] = topping
-    return result
-  }, {})
-  orders.forEach(function (order) {
-    order.orderItems.forEach(function (orderItem) {
-      orderItem.toppings = orderItem.toppingIds.map(function (_id) {
-        // console.log(indexed[_id.toString()])
-        return indexed[_id.toString()]
-      })
-    })
-  })
-  return orders
-}
-
 router.get('/', function(req, res, next) {
   var orders;
 
@@ -73,7 +20,7 @@ router.get('/', function(req, res, next) {
       return order.userId
     })
     Users.find({_id: {$in: userIds}}).then(function (ordersUsers) {
-      joinOrderUser(orders, ordersUsers)
+      OrdersFunctions.joinOrderUser(orders, ordersUsers)
 
       var orderItemIds = orders.reduce(function (result, order) {
         return result.concat(order.orderItemIds)
@@ -102,8 +49,8 @@ router.get('/', function(req, res, next) {
           // console.log(orders[0].orderItemIds)
           // console.log(orders[0].orderItemIds.pizzas)
       Pizzas.find({_id: {$in: orderItemIds}}).then(function (orderItems) {
-        joinOrderItems(orders, orderItems)
-// // logs
+        OrdersFunctions.joinOrderItems(orders, orderItems)
+// logs
 //           // console.log('***** orders *****')
 //           // console.log(orders)        
 //                 // returns object -> Order
@@ -129,10 +76,10 @@ router.get('/', function(req, res, next) {
         return result.concat(topping.toppingIds)
       }, [])
       Toppings.find({_id: {$in: toppingIds}}).then(function (toppings) {  
-        joinOrderItemsToppings(orders, toppings)
-// // logs
-//             // console.log('***** orders[0].orderItems[0].toppings[0].name *****')          
-//             // console.log(orders[0].orderItems[0].toppings[0].name)
+        OrdersFunctions.joinOrderItemsToppings(orders, toppings)
+// logs
+            // console.log('***** orders[0].orderItems[0].toppings[0].name *****')          
+            // console.log(orders[0].orderItems[0].toppings[0].name)
         res.render('orders/index', {title: 'All Orders',
                                     orders: orders
                                   })
